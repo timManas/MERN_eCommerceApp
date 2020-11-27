8cd820340452bae435515f6f1ad2dbc0b1a50db4
TimManas <timothy.romero.manas@gmail.com>
2020-11-27 11:51:08 -0500

    React-BootStrap Setup, Header and Footer, Fonts and themes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap' // We import Containers from react-bootstrap
+import Header from './components/Header' // If Header does not contain "default" in export, then you need to do {}
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
