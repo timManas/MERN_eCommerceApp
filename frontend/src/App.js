8c0de2094cf0cdbfd363b995c93c0c37fade8ee9
TimManas <timothy.romero.manas@gmail.com>
2020-11-30 21:30:42 -0500

    Homescreen Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap' // We import Containers from react-bootstrap
 import Header from './components/Header' // If Header does not contain "default" in export, then you need to do {}
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
