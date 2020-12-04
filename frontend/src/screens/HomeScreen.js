a776e1c29d3101da0fb9c4fd1956acb97e174c41
TimManas <timothy.romero.manas@gmail.com>
2020-12-04 09:15:47 -0500

    Fetching Products from React - Backend + FrontEnd

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]) // Use state
+
+  // Note: Right when the HomeScreen loads, it will make a request to the backend Server for products
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+
   return (
     <>
       <h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import {
   Row,
@@ -10,10 +10,23 @@ import {
   ListGroupItem,
 } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
+// import products from '../products'
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id) // What is p ? It is referring to current p index in the products file
+  // Note: Comment this out since we will be fetching from the backend
+  // const product = products.find((p) => p._id === match.params.id) // What is p ? It is referring to current p index in the products file
+
+  const [product, setProduct] = useState([])
+  // Note: Right when the HomeScreen loads, it will make a request to the backend Server for products
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [])
 
   return (
     <>
