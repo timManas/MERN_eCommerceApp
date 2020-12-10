d873bcc2b1487d1ca5135fee6228808bcd011266
TimManas <timothy.romero.manas@gmail.com>
2020-12-10 18:36:23 -0500

    Added documentation

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-      .bold
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+      .inverse
   )
 )
