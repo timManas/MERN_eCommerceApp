dd2e54243233a25d1776583e6361c25131fe2b0c
TimManas <timothy.romero.manas@gmail.com>
2020-12-13 18:08:25 -0500

    Brining redux to home screen and fixing bug

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,36 @@
 // Note: Actions are objects containing a type and payload
 // Used to tell reducer how to update the store
+// import axios from 'axios'
+// import {
+//   PRODUCT_LIST_REQUEST,
+//   PRODUCT_LIST_SUCCESS,
+//   PRODUCT_LIST_FAIL,
+// } from '../constants/productConstants'
+
+// export const listProducts = () => async (dispatch) => {
+//   try {
+//     dispatch({ type: PRODUCT_LIST_REQUEST }) // Sending request`
+
+//     const { data } = await axios.get('/api/products')
+
+//     // At this point, we have the product data from the backend
+//     dispatch({
+//       // Fetching the data was successful
+//       type: PRODUCT_LIST_SUCCESS,
+//       payload: data,
+//     })
+//   } catch (error) {
+//     // If fetching data was not succesful
+//     dispatch({
+//       type: PRODUCT_LIST_FAIL,
+//       payload:
+//         error.response && error.response.data.message
+//           ? error.response.data.message
+//           : error.message,
+//     })
+//   }
+// }
+
 import axios from 'axios'
 import {
   PRODUCT_LIST_REQUEST,
@@ -9,18 +40,15 @@ import {
 
 export const listProducts = () => async (dispatch) => {
   try {
-    dispatch({ type: PRODUCT_LIST_REQUEST }) // Sending request`
+    dispatch({ type: PRODUCT_LIST_REQUEST })
 
     const { data } = await axios.get('/api/products')
 
-    // At this point, we have the product data from the backend
     dispatch({
-      // Fetching the data was successful
       type: PRODUCT_LIST_SUCCESS,
       payload: data,
     })
   } catch (error) {
-    // If fetching data was not succesful
     dispatch({
       type: PRODUCT_LIST_FAIL,
       payload:
