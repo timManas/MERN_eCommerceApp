7bcbd56b645341d0cf3943880e6629cd55cffe26
TimManas <timothy.romero.manas@gmail.com>
2020-12-16 19:41:54 -0500

    Custom Authentication Middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+// Note: This middleware validates the token
+import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+const protect = asyncHandler(async (req, res, next) => {
+  // We are putting the token in the header
+  let token
+
+  // Check if token is valid and found
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1] // Fetch the token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET) // Verify the token
+      console.log('Decoded: ' + decoded)
+
+      req.user = await User.findById(decoded.id).select('-password') // Gets all user request and places it req.user
+
+      next()
+    } catch (error) {
+      console.error(error)
+      res.status(401)
+      throw new Error('Not Authorized, token failed')
+    }
+  }
+
+  // If token DNE
+  if (!token) {
+    res.status(401)
+    throw new Error('Not Authorized, no token')
+  }
+})
+
+export { protect }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { authUser } from '../controllers/userController.js'
+import { authUser, getUserProfile } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 // GET list of products in JSON format
 router.post('/login', authUser)
+
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
