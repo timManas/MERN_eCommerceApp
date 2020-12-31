788e734f0814537ded47c3d5d0fcb35dc5704e2a
TimManas <timothy.romero.manas@gmail.com>
2020-12-31 14:32:31 -0500

    Prepaer for deployment'

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,22 @@ app.get(
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+// RUN THIS ONLY IN PRODUCTION MODE
+if (process.env.NODE_ENV === 'production') {
+  // Set the 'build' folder as a static folder
+  // Why ? So we can access the build folder and load the index.html
+  app.use(express.static(path.join(__dirname, '/frontend/build'))) // set frontend folder as a static folder
+
+  // Send the index.html file if on Production
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running ....')
+  })
+}
+
 // Error - 404 handling
 app.use(notFound)
 
